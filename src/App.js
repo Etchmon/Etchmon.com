229e34bbc0de42243a7d9546bf61058ec409f04c
James Etchells <etch.rush@gmail.com>
2023-06-01 12:51:23 -0700

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.js 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
 function App() {
 
+  const toggleNav = () => {
+    document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
+  }
 
   return (
-    <main data-nav="false">
-      <button id="nav-toggle" type="button">
-
+    <section data-nav="false">
+      <main></main>
+      <nav id='nav'>
+        <div className='nav-links' >
+          <a className='nav-link' href="#">
+            <h2 className='nav-link-label'>Home</h2>
+            <img className='nav-link-image' src='' />
+          </a>
+          <a className='nav-link' href="#">
+            <h2 className='nav-link-label'>Resume</h2>
+            <img className='nav-link-image' src='' />
+          </a>
+          <a className='nav-link' href="#">
+            <h2 className='nav-link-label'>Projects</h2>
+            <img className='nav-link-image' src='' />
+          </a>
+          <a className='nav-link' href="#">
+            <h2 className='nav-link-label'>Projects</h2>
+            <img className='nav-link-image' src='' />
+          </a>
+          <a className='nav-link' href="#">
+            <h2 className='nav-link-label'>Projects</h2>
+            <img className='nav-link-image' src='' />
+          </a>
+        </div>
+      </nav>
+      <button id="nav-toggle" type="button" onClick={toggleNav}>
+        <MenuIcon className='open i' />
+        <CloseIcon className='close i' />
       </button>
-    </main>
+    </section>
+
   );
 }
 
