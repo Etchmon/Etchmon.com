748dd2e6434a7a19d35a4e4fdf102164e8153a01
James Etchells <etch.rush@gmail.com>
2023-06-05 10:22:25 -0700

    modified:   src/App.js 	new file:   src/components/about.js 	new file:   src/components/home.js 	new file:   src/components/projects.js 	new file:   src/components/resume.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import imgSrc from '../src/images/keira.jpg';
+import Home from '../src/components/home';
 
 function App() {
 
   const scrollableRef = useRef(null);
+  const [activeComponent, setActiveComponent] = useState('home');
+
+  const renderComponent = () => {
+    switch (activeComponent) {
+      case 'home':
+        return <Home />
+      case 'skills':
+        return // skills page
+      case 'projects':
+        return // projects page
+      case 'resume':
+        return // resume
+      case 'about':
+        return // about
+      default:
+        return null;
+    }
+  }
 
   const toggleNav = () => {
     document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
@@ -17,44 +36,27 @@ function App() {
 
   return (
     <section data-nav="false">
-      <main>
-        <header>
-          <h1><span>James</span> Etchells</h1>
-          <h3>Full-Stack Software Engineer</h3>
-          <ul>
-            <li>
-              <a href='https://github.com/Etchmon' target='_blank'>Github</a>
-            </li>
-            <li>
-              <a href='https://www.linkedin.com/in/james-etchells-240816164/' target='_blank'>LinkedIn</a>
-            </li>
-            <li>
-              <a href='mailto:jaw.etch@gmail.com' target='_blank'>Email</a>
-            </li>
-          </ul>
-        </header>
-        <div></div>
-      </main>
+      {renderComponent()}
 
       <nav id='nav'>
         <div id='nav-links' className='scrollbar-hidden' onWheel={handleWheel} ref={scrollableRef}>
-          <a className='nav-link' href="#">
+          <a className='nav-link' onClick={() => setActiveComponent('home')}>
             <h2 className='nav-link-label'>Home</h2>
             <img className='nav-link-image' src={imgSrc} />
           </a>
-          <a className='nav-link' href="#">
+          <a className='nav-link' onClick={() => setActiveComponent('skills')}>
             <h2 className='nav-link-label'>Skills</h2>
             <img className='nav-link-image' src={imgSrc} />
           </a>
-          <a className='nav-link' href="#">
+          <a className='nav-link' onClick={() => setActiveComponent('projects')}>
             <h2 className='nav-link-label'>Projects</h2>
             <img className='nav-link-image' src={imgSrc} />
           </a>
-          <a className='nav-link' href="#">
+          <a className='nav-link' onClick={() => setActiveComponent('resume')}>
             <h2 className='nav-link-label'>Resume</h2>
             <img className='nav-link-image' src={imgSrc} />
           </a>
-          <a className='nav-link' href="#">
+          <a className='nav-link' onClick={() => setActiveComponent('about')}>
             <h2 className='nav-link-label'>About</h2>
             <img className='nav-link-image' src={imgSrc} />
           </a>
