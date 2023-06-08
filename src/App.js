14bae9b55ec5891173a4222ea80ac5a2b7e86535
James Etchells <etch.rush@gmail.com>
2023-06-08 11:46:05 -0700

    modified:   src/App.js 	new file:   src/images/bg-img.jpg 	deleted:    src/images/simon-fitall-tvleqH3p1os-unsplash.jpg 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import imgSrc1 from '../src/images/localhost_3000_.png';
@@ -12,8 +12,13 @@ import About from './components/about';
 
 function App() {
 
+  const [pageLoad, setPageLoad] = useState(false)
   const scrollableRef = useRef(null);
   const [activeComponent, setActiveComponent] = useState('home');
+
+  useEffect(() => {
+    window.onload = setPageLoad(true);
+  })
 
   const toggleNav = () => {
     document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
@@ -37,6 +42,7 @@ function App() {
 
   const handleWheel = (event) => {
     scrollableRef.current.scrollLeft += event.deltaY + event.deltaX;
+
   };
 
   return (
@@ -48,22 +54,22 @@ function App() {
 
       <nav id='nav'>
         <div id='nav-links' className='scrollbar-hidden' onWheel={handleWheel} ref={scrollableRef}>
-          <a className='nav-link' onClick={() => setActiveComponent('home')}>
+          <div className='nav-link' onClick={() => setActiveComponent('home')}>
             <h2 className='nav-link-label'>Home</h2>
-            <img className='nav-link-image' src={imgSrc1} />
-          </a>
-          <a className='nav-link' onClick={() => setActiveComponent('skills')}>
+            <img className='nav-link-image' src={imgSrc1} alt='Home' />div
+          </div>
+          <div className='nav-link' onClick={() => setActiveComponent('skills')}>
             <h2 className='nav-link-label'>Skills</h2>
-            <img className='nav-link-image' src={imgSrc2} />
-          </a>
-          <a className='nav-link' onClick={() => setActiveComponent('projects')}>
+            <img className='nav-link-image' src={imgSrc2} alt='Skills' />
+          </div>
+          <div className='nav-link' onClick={() => setActiveComponent('projects')}>
             <h2 className='nav-link-label'>Projects</h2>
-            <img className='nav-link-image' src={imgSrc3} />
-          </a>
-          <a className='nav-link' onClick={() => { setActiveComponent('about') }}>
+            <img className='nav-link-image' src={imgSrc3} alt='Projects' />
+          </div>
+          <div className='nav-link' onClick={() => { setActiveComponent('about') }}>
             <h2 className='nav-link-label'>About</h2>
-            <img className='nav-link-image' src={imgSrc4} />
-          </a>
+            <img className='nav-link-image' src={imgSrc4} alt='About' />
+          </div>
         </div>
       </nav>
       <button id="nav-toggle" type="button" onClick={toggleNav}>
